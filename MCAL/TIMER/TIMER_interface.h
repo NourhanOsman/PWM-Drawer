c22db3656b06a484f79de03a982082cd9d2aa7cf
NourhanOsman <153376227+NourhanOsman@users.noreply.github.com>
2023-12-09 20:18:06 +0200

    Update TIMER_interface.h

--- MCAL/TIMER/TIMER_interface.h
+++ MCAL/TIMER/TIMER_interface.h
@@ -19,7 +19,6 @@ void TIMER0_voidInit(void);
 u8 TIMER0_callBack(u8 copy_u8IntID,void (*ptrToFunc)(void));
 
 void TIMER1_voidInit(void);
-void TIMER1_voidSetCompareValueChannelA(u16 copy_u16CompareValue);
 
 void TIMER1_voidSetTimerValue(u16 copy_u16TimerValue);
 u16 TIMER1_voidGetTimerValue(void);
