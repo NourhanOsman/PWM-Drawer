e113d5447e74d91829dfe8f3265347220982511b
NourhanOsman <153376227+NourhanOsman@users.noreply.github.com>
2023-12-09 20:05:36 +0200

    Add files via upload

---
+++ MCAL/TIMER/TIMER_private.h
@@ -0,0 +1,46 @@
+/***********************************************************************/
+/***********************************************************************/
+/********************  Author: Nourhan Osman   *************************/
+/********************  SWC: TIMERS             *************************/
+/********************  LAYER: MCAL             *************************/
+/********************  DATE: 9/12/2023         *************************/
+/***********************************************************************/
+/***********************************************************************/
+
+#ifndef TIMRE_PRIVATE_H_
+#define TIMRE_PRIVATE_H_
+
+// PRESCALAR
+#define TIMER_PRESCALER_1     1
+#define TIMER_PRESCALER_8     2
+#define TIMER_PRESCALER_64    3
+#define TIMER_PRESCALER_256   4
+#define TIMER_PRESCALER_1024  5
+
+
+
+#define TMR_WGM00   6
+#define TMR_WGM01   3
+#define TMR_OCIE0   1
+#define TMR_TOIE0   0
+
+#define TMR_COM00   4
+#define TMR_COM01   5
+
+#define TIMER1_COM1A1          7
+#define TIMER1_COM1A0          6
+#define TIMER1_COM1B1          5
+#define TIMER1_COM1B0          4
+#define TIMER1_WGM11           1
+#define TIMER1_WGM10           0
+
+#define TIMER1_WGM13           4
+#define TIMER1_WGM12           3
+#define TIMER1_CS12            2
+#define TIMER1_CS11            1
+#define TIMER1_CS10            0
+
+
+
+
+#endif
---
+++ MCAL/TIMER/TIMER_register.h
@@ -0,0 +1,32 @@
+/***********************************************************************/
+/***********************************************************************/
+/********************  Author: Nourhan Osman   *************************/
+/********************  SWC: TIMERS             *************************/
+/********************  LAYER: MCAL             *************************/
+/********************  DATE: 9/12/2023         *************************/
+/***********************************************************************/
+/***********************************************************************/
+
+#ifndef TIMER_REGISTER_H_
+#define TIMER_REGISTER_H_
+					        
+#define TIMER_OCR0          (*((volatile u8*)0X5C))
+#define TIMER_TCCR0         (*((volatile u8*)0X53))
+#define TIMER_TCNT0         (*((volatile u8*)0X52))
+#define TIMER_TIMSK         (*((volatile u8*)0X59))
+#define TIMER_TIFR          (*((volatile u8*)0X58))
+
+/*TIMER 1*/
+#define TIMER1_TCCR1A       (*((volatile u8*)0X4F))
+    
+
+
+#define TIMER1_TCCR1B     (*((volatile u8*)0X4E))
+	  
+#define TIMER1_OCR1A     (*((volatile u16*)0X4A))
+#define TIMER1_ICR1      (*((volatile u16*)0X46))
+#define TIMER1_TCNT1     (*((volatile u16*)0X4C))
+		
+#endif	
+		
+		
